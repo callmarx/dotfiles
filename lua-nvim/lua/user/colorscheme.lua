f78d79ee56965f7ec2346625d2eca2e92f2a7ab2
Eugenio Augusto Jimenes <eugeniojimenes@gmail.com>
2022-03-12 15:52:48 -0300

    feat(lua): add nightfox theme plugin

--- lua-nvim/lua/user/colorscheme.lua
+++ lua-nvim/lua/user/colorscheme.lua
@@ -1,8 +1,27 @@
-vim.cmd [[
-try
-  colorscheme darkplus
-catch /^Vim\%((\a\+)\)\=:E185/
-  colorscheme default
-  set background=dark
-endtry
-]]
+local status_ok, nightfox = pcall(require, "nightfox")
+if not status_ok then
+  return
+end
+
+nightfox.setup {
+  fox = "duskfox", -- Which fox style should be applied
+  transparent = false, -- Disable setting the background color
+  alt_nc = false, -- Non current window bg to alt color see `hl-NormalNC`
+  terminal_colors = true, -- Configure the colors used when opening :terminal
+  styles = {
+    comments = "NONE", -- Style that is applied to comments: see `highlight-args` for options
+    functions = "NONE", -- Style that is applied to functions: see `highlight-args` for options
+    keywords = "NONE", -- Style that is applied to keywords: see `highlight-args` for options
+    strings = "NONE", -- Style that is applied to strings: see `highlight-args` for options
+    variables = "NONE", -- Style that is applied to variables: see `highlight-args` for options
+  },
+  inverse = {
+    match_paren = false, -- Enable/Disable inverse highlighting for match parens
+    visual = false, -- Enable/Disable inverse highlighting for visual selection
+    search = false, -- Enable/Disable inverse highlights for search highlights
+  },
+  colors = {}, -- Override default colors
+  hlgroups = {}, -- Override highlight groups
+}
+
+nightfox.load()
