fd077725496a529c5fd576477b5ffedada2e9886
Eugenio Augusto Jimenes <eugeniojimenes@gmail.com>
2022-03-12 02:21:42 -0300

    feat(lua): fix github-theme config
     - move configs from plugin.lua to colorscheme.lua and fix it

--- lua-nvim/lua/user/plugins.lua
+++ lua-nvim/lua/user/plugins.lua
@@ -48,15 +48,7 @@ return packer.startup(function(use)
   -- Colorschemes
   -- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
   -- use "lunarvim/darkplus.nvim"
-  use {
-    "projekt0n/github-nvim-theme",
-    config = function()
-      require("github-theme").setup({
-        theme_style = "dimmed"
-        -- your github config
-      })
-    end
-  }
+  use "projekt0n/github-nvim-theme"
 
   -- cmp plugins
   use "hrsh7th/nvim-cmp" -- The completion plugin
