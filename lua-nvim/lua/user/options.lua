ff44d8a587cb81e404ec170cc7abe092c91d4bb5
Eugenio Augusto Jimenes <eugeniojimenes@gmail.com>
2022-03-14 19:46:32 -0300

    feat(lua): change options cmp mapping confirm
     - add some options in lua/user/options.lua and order the list
       alphabetically
     - set 'select = false' to only confirm explicitly selected item
       in cmp

--- lua-nvim/lua/user/options.lua
+++ lua-nvim/lua/user/options.lua
@@ -5,35 +5,39 @@ local options = {
   colorcolumn = "100",                     -- set a light grey column at character 100
   completeopt = { "menuone", "noselect" }, -- mostly just for cmp
   conceallevel = 0,                        -- so that `` is visible in markdown files
+  cursorline = true,                       -- highlight the current line
+  expandtab = true,                        -- convert tabs to spaces
   fileencoding = "utf-8",                  -- the encoding written to a file
+  guifont = "monospace:h17",               -- the font used in graphical neovim applications
   hlsearch = true,                         -- highlight all matches on previous search pattern
-  ignorecase = true,                       -- ignore case in search patterns
---  mouse = "a",                             -- allow the mouse to be used in neovim
+  ignorecase = false,                      -- use case sensitive in search patterns
+  list = true,                             -- display invisible characters
+  -- listchars = { tab = "▸ ", trail = "·" },
+  -- mouse = "a",                             -- allow the mouse to be used in neovim
+  number = true,                           -- set numbered lines
+  numberwidth = 4,                         -- set number column width to 2 {default 4}
   pumheight = 10,                          -- pop up menu height
+  relativenumber = true,                   -- set relative numbered lines
+  scrolloff = 8,                           -- as you go down or up in lines it automatically scroll at 8 lines
+  shiftwidth = 2,                          -- the number of spaces inserted for each indentation
   showmode = false,                        -- we don't need to see things like -- INSERT -- anymore
   showtabline = 2,                         -- always show tabs
+  sidescrolloff = 8,                       -- as you go right or left in the line it automatically scroll horizontally at 8 characters
+  signcolumn = "yes",                      -- always show the sign column, otherwise it would shift the text each time
   smartcase = true,                        -- smart case
   smartindent = true,                      -- make indenting smarter again
   splitbelow = true,                       -- force all horizontal splits to go below current window
   splitright = true,                       -- force all vertical splits to go to the right of current window
   swapfile = false,                        -- creates a swapfile
+  tabstop = 2,                             -- insert 2 spaces for a tab
   termguicolors = true,                    -- set term gui colors (most terminals support this)
   timeoutlen = 500,                        -- time to wait for a mapped sequence to complete (in milliseconds)
   undofile = true,                         -- enable persistent undo
   updatetime = 300,                        -- faster completion (4000ms default)
-  writebackup = false,                     -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
-  expandtab = true,                        -- convert tabs to spaces
-  shiftwidth = 2,                          -- the number of spaces inserted for each indentation
-  tabstop = 2,                             -- insert 2 spaces for a tab
-  cursorline = true,                       -- highlight the current line
-  number = true,                           -- set numbered lines
-  relativenumber = true,                   -- set relative numbered lines
-  numberwidth = 4,                         -- set number column width to 2 {default 4}
-  signcolumn = "yes",                      -- always show the sign column, otherwise it would shift the text each time
+  wildmenu = true,                         -- command line completion
+  wildmode = "longest:full,full",
   wrap = false,                            -- display lines as one long line
-  scrolloff = 8,                           -- as you go down or up in lines it automatically scroll at 8 lines
-  sidescrolloff = 8,                       -- as you go right or left in the line it automatically scroll horizontally at 8 characters
-  guifont = "monospace:h17",               -- the font used in graphical neovim applications
+  writebackup = false,                     -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
 }
 
 vim.opt.shortmess:append "c"
@@ -44,4 +48,3 @@ end
 
 vim.cmd "set whichwrap+=<,>,[,],h,l"
 vim.cmd [[set iskeyword+=-]]
-vim.cmd [[set formatoptions-=cro]] -- TODO: this doesn't seem to work
--- lua-nvim/lua/user/plugins.lua
+++ lua-nvim/lua/user/plugins.lua
@@ -45,7 +45,7 @@ return packer.startup(function(use)
   use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
   use "windwp/nvim-autopairs" -- Autopairs, integrates with both cmp and treesitter
-  use {                          -- I still prefer this plugin :)
+  use {                       -- I still prefer this plugin for commenting :)
     "preservim/nerdcommenter",
     config = function()
       vim.g.NERDDefaultAlign = "left"
