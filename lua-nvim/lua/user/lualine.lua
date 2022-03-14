c4d6f9f4958e9a4d3d38e25279cf05dfae32f585
Eugenio Augusto Jimenes <eugeniojimenes@gmail.com>
2022-03-14 12:51:03 -0300

    feat(lua): change lualine filename config

--- lua-nvim/lua/user/lualine.lua
+++ lua-nvim/lua/user/lualine.lua
@@ -7,4 +7,23 @@ lualine.setup({
 	options = {
 		theme = "codedark",
 	},
+  sections = {
+    lualine_c = {
+      {
+        'filename',
+        file_status = true,      -- Displays file status (readonly status, modified status)
+        path = 1,                -- 0: Just the filename
+                                 -- 1: Relative path
+                                 -- 2: Absolute path
+
+        shorting_target = 40,    -- Shortens path to leave 40 spaces in the window
+                                 -- for other components. (terrible name, any suggestions?)
+        symbols = {
+          modified = '[+]',      -- Text to show when the file is modified.
+          readonly = '[-]',      -- Text to show when the file is non-modifiable or readonly.
+          unnamed = '[No Name]', -- Text to show for unnamed buffers.
+        }
+      }
+    }
+  }
 })
