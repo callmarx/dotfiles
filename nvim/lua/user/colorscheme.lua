10f305d41a46fd971823580c021d401c9a9a6538
Eugenio Augusto Jimenes <eugeniojimenes@gmail.com>
2022-06-27 14:54:54 -0300

    fix(treesitter): specify languages to install
     - Also fixed kanagawa.setup required after update

--- nvim/lua/user/colorscheme.lua
+++ nvim/lua/user/colorscheme.lua
@@ -5,12 +5,12 @@ end
 
 kanagawa.setup {
   undercurl = true,           -- enable undercurls
-  commentStyle = "italic",
-  functionStyle = "NONE",
-  keywordStyle = "italic",
-  statementStyle = "bold",
-  typeStyle = "NONE",
-  variablebuiltinStyle = "italic",
+  commentStyle = { italic = true },
+  functionStyle = {},
+  keywordStyle = { italic = true },
+  statementStyle = { bold = true },
+  typeStyle = {},
+  variablebuiltinStyle = { italic = true },
   specialReturn = true,       -- special highlight for the return keyword
   specialException = true,    -- special highlight for exception handling keywords
   transparent = false,        -- do not set background color
--- nvim/lua/user/treesitter.lua
+++ nvim/lua/user/treesitter.lua
@@ -1,15 +1,23 @@
+-- Source: https://github.com/nvim-treesitter/nvim-treesitter
+-- "Nvim-treesitter is based on three interlocking features: language parsers,
+-- queries, and modules, where modules provide features – e.g., highlighting"
+
 local status_ok, configs = pcall(require, "nvim-treesitter.configs")
 if not status_ok then
   return
 end
 
 configs.setup {
-  ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  ensure_installed = {
+    "lua", "bash", "make", "dockerfile", "regex",
+    "html", "css", "scss", "json", "markdown", "yaml", "embedded_template",
+    "ruby", "python", "javascript", "typescript",
+  }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
   ignore_install = { "" }, -- List of parsers to ignore installing
   autopairs = {
-		enable = true,
-	},
+    enable = true,
+  },
   highlight = {
     enable = true, -- false will disable the whole extension
     disable = { "" }, -- list of language that will be disabled
